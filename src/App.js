81f9ea0aa0f64ec8779c4bb9ed8655c662aba06b
Mac <tmcpbmay@gmail.com>
2020-10-05 21:50:27 -0400

    forgot a file

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import Instructions from './Instructions'
 import './App.css';
 
 function App() {
-  const [ack, setAck] = useState(true);
+  const [ack, setAck] = useState(true); // Player acknowledges they've read the rules
   const [difficulty, setDifficulty] = useState(-1)
 
   return (
     <div className="App">
-      {ack ? <Instructions ack={setAck} /> : null}
+      {/* Okay so obviously now I know I can just use a router to make the instructions go away */}
+      {ack ? <Instructions ack={setAck} /> : null} 
       {difficulty >= 0 ? null : <Difficulty set={setDifficulty} />}
       <Game difficulty={difficulty} />
     </div>
